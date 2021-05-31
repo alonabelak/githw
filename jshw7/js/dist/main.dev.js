0ee8f115256b192d06945d7410a2683f68001407
Alona <alyonabelak@gmail.com>
2021-05-31 17:47:05 +0300

    js classes

--- jshw7/js/dist/main.dev.js
+++ jshw7/js/dist/main.dev.js
@@ -1,5 +1,13 @@
 "use strict";
 
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -19,14 +27,12 @@ function () {
     key: "areaCircle",
     value: function areaCircle() {
       var s = (Math.PI * Math.pow(this.radius, 2)).toFixed(2);
-      document.write("<p>Area: ".concat(s, "</p>"));
       return s;
     }
   }, {
-    key: "cicleLength",
-    value: function cicleLength() {
+    key: "circleLength",
+    value: function circleLength() {
       var l = (2 * Math.PI * this.radius).toFixed(2);
-      document.write("<p>Circle length: ".concat(P, "</p>"));
       return l;
     }
   }, {
@@ -41,6 +47,7 @@ function () {
     key: "diameterInfo",
     get: function get() {
       var d = (2 * this.radius).toFixed(2);
+      return d;
     }
   }]);
 
@@ -50,3 +57,93 @@ function () {
 var circle = new Circle({
   radius: 3
 });
+console.log(circle.radiusInfo);
+console.log(circle.diameterInfo);
+console.log(circle.areaCircle());
+console.log(circle.circleLength());
+
+var Marker =
+/*#__PURE__*/
+function () {
+  function Marker(options) {
+    _classCallCheck(this, Marker);
+
+    this.color = options.color;
+    this.quantity = options.quantity;
+  }
+
+  _createClass(Marker, [{
+    key: "print",
+    value: function print(text) {
+      var style = "color: ".concat(this.color);
+      return "<p style=\"".concat(style, "\">").concat(text, "</p>");
+    }
+  }]);
+
+  return Marker;
+}();
+
+var greenText = new Marker({
+  color: "green",
+  quantity: 100
+});
+
+function styleInfo() {
+  var text = document.getElementById("print").value;
+  var style = "";
+  style += "color:".concat(greenText.color);
+  var rez = "<p style=\"".concat(style, "\">").concat(text, "</p>");
+  document.getElementById("style_rez").innerHTML = rez;
+}
+
+var Employee = function Employee(name, age, experience, department) {
+  _classCallCheck(this, Employee);
+
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+  this.department = department;
+};
+
+var employees = [new Employee("John", "25", "2", "Marketing"), new Employee("Julia", "22", "1", "Sales"), new Employee("Ben", "27", "5", "Accounting"), new Employee("Lesia", "30", "3", "HR")];
+
+var EmpTable =
+/*#__PURE__*/
+function () {
+  function EmpTable(employees) {
+    _classCallCheck(this, EmpTable);
+
+    this.employees = employees;
+  }
+
+  _createClass(EmpTable, [{
+    key: "getHtml",
+    value: function getHtml() {
+      document.write("<table border = \"3\"><tr>");
+
+      for (var _i = 0, _Object$entries = Object.entries(employees[0]); _i < _Object$entries.length; _i++) {
+        var _Object$entries$_i = _slicedToArray(_Object$entries[_i], 1),
+            key = _Object$entries$_i[0];
+
+        document.write('<th>' + key + '</th>');
+      }
+
+      document.write("</tr>");
+
+      for (var i = 0; i < employees.length; i++) {
+        document.write("<tr>");
+        Object.values(employees[i]).forEach(function (empl) {
+          document.write("<td>" + empl + "</td>");
+        });
+        document.write("</tr>");
+      }
+
+      document.write("</table>");
+    }
+  }]);
+
+  return EmpTable;
+}();
+
+var empTable = new EmpTable(employees);
+empTable.getHtml();
--- jshw7/js/main.js
+++ jshw7/js/main.js
@@ -10,19 +10,84 @@ class Circle {
     }
     get diameterInfo(){
         let d = (2 * this.radius).toFixed(2);
-
+        return d;
     }
     areaCircle() {
         let s = (Math.PI * Math.pow(this.radius, 2)).toFixed(2);
-        document.write(`<p>Area: ${s}</p>`);
         return s;
     }
-    cicleLength() {
+    circleLength() {
         let l = (2 * Math.PI * this.radius).toFixed(2);
-        document.write(`<p>Circle length: ${P}</p>`);
         return l;
     }
 }
 const circle = new Circle({
     radius: 3
 });
+
+console.log(circle.radiusInfo)
+console.log(circle.diameterInfo);
+console.log(circle.areaCircle());
+console.log(circle.circleLength());
+
+class Marker {
+    constructor (options){
+        this.color = options.color;
+        this.quantity = options.quantity;
+    }
+    print(text){
+        let style = `color: ${this.color}`;
+        return(`<p style="${style}">${text}</p>`);
+    }   
+}
+const greenText = new Marker({
+    color: "green",
+    quantity: 100
+});
+function styleInfo (){ 
+    let text = document.getElementById("print").value;
+    let style = "";
+    style += `color:${greenText.color}`;   
+    let rez = `<p style="${style}">${text}</p>`;
+    document.getElementById("style_rez").innerHTML = rez;
+}      
+
+class Employee {
+    constructor (name, age, experience, department){
+        this.name = name;
+        this.age = age;
+        this.experience = experience;
+        this.department = department;
+    }
+}
+const employees = [
+    new Employee("John","25","2","Marketing"), 
+    new Employee("Julia","22","1","Sales"), 
+    new Employee("Ben","27","5","Accounting"), 
+    new Employee("Lesia","30","3","HR")
+];
+
+class EmpTable {
+    constructor(employees) {
+      this.employees = employees;
+    }
+   
+    getHtml() { 
+      document.write("<table border = \"3\"><tr>");
+      for (const [key] of Object.entries(employees[0])) {
+        document.write('<th>' + key + '</th>');
+      }
+      document.write("</tr>");
+   
+      for (let i = 0; i < employees.length; i++) {
+        document.write("<tr>");
+        Object.values(employees[i]).forEach(empl => {
+          document.write("<td>" + empl + "</td>");
+        });
+        document.write("</tr>");
+      }
+      document.write("</table>");
+    }
+}
+const empTable = new EmpTable(employees);
+empTable.getHtml();
